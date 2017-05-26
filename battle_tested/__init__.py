023e560da2fdf03119b7410c3e5bfab69dfdedde
Cody Kochmann <kochmanncody@gmail.com>
2017-05-26 13:41:40 -0400

    third attempt at better imports

---
+++ battle_tested/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-04-27 12:49:17
+# @Last Modified by:   cody
+# @Last Modified time: 2017-05-26 13:40:48
+
+"""
+battle_tested - automated function fuzzer based on hypothesis to easily test production code
+
+Example Usage:
+
+    from battle_tested import battle_tested
+
+    def test_function(a,b,c):
+        return c,b,a
+
+    battle_tested(test_function)
+
+Or:
+
+    from battle_tested import battle_tested
+
+    @battle_tested()
+    def test_function(a,b,c):
+        return c,b,a
+
+"""
