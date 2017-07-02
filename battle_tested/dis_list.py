1b4de3daf1ee8e7ef67b9a4cbddad8330d1734a4
Cody Kochmann <kochmanncody@gmail.com>
2017-07-02 12:55:16 -0400

    forgot to add dis_list.py to make the initial demo work

---
+++ battle_tested/dis_list.py
@@ -0,0 +1,28 @@
+from dis import dis
+from io import StringIO
+import sys
+
+def f(a,b):
+    return b,a
+
+def return_stdout(fn):
+    _old = sys.stdout
+    out = StringIO()
+    sys.stdout=out
+    fn()
+    sys.stdout=_old
+    return out.getvalue()
+
+def dis_list(fn): # lolololololol
+    return [i[10:] for i in return_stdout(lambda:dis(fn)).splitlines() if len(i)>10]
+
+def dis_calls(fn):
+    return [i.strip().split(' ')[1] for i in dis_list(fn)]
+
+for i in dis_list(f):
+    print(i)
+    
+for c in dis_calls(f):
+    print(c)
+
+
