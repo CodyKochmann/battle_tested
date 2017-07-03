d6ed43b9fc81a45ca5204b041c503b94b7df5eaa
Cody Kochmann <kochmanncody@gmail.com>
2017-07-03 11:28:09 -0400

    added stricttuple to the setup.py requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
   version = '2017.6.30',
-  install_requires=["hypothesis"],
+  install_requires=["hypothesis", "stricttuple"],
   description = 'automated function fuzzer based on hypothesis to easily test production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
