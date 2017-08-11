1a3d7ce369dc5c39df6a605e1628b11046b0cfa3
Cody Kochmann <kochmanncody@gmail.com>
2017-08-11 16:53:26 -0400

    switched to latest hypothesis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
   version = '2017.8.11.2',
-  install_requires = ["hypothesis==3.13.1", "stricttuple", "prettytable"],
+  install_requires = ["hypothesis", "stricttuple", "prettytable"],
   description = 'automated function fuzzer based on hypothesis to easily test production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
