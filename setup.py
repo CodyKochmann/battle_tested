7aaa95eff924a278f9cfdcb8ff2fce57dd738a03
Cody Kochmann <kochmanncody@gmail.com>
2017-09-08 16:14:58 -0400

    added generator support

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
-  version = '2017.9.7.2',
+  version = '2017.9.8',
   install_requires = ["hypothesis", "stricttuple", "prettytable"],
   description = 'automated function fuzzer based on hypothesis to easily test production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/battle_tested',
-  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2017.9.7.2',
+  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2017.9.8',
   keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production'],
   classifiers = [],
 )
