a7512d6350fbbb2be45c441c26d21eb766993709
Cody Kochmann <kochmanncody@gmail.com>
2017-09-26 18:52:14 -0400

    added generators to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
   version = '2017.9.26',
-  install_requires = ["hypothesis", "stricttuple", "prettytable"],
+  install_requires = ["hypothesis", "stricttuple", "prettytable", "generators"],
   description = 'automated function fuzzer based on hypothesis to easily test production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
