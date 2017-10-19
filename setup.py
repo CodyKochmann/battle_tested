1dd8355fbc9ece31bddf6d34c9cf8d11560b59e7
Cody Kochmann <kochmanncody@gmail.com>
2017-10-19 13:29:28 -0400

    new release!

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
-  version = '2017.10.18.1',
+  version = '2017.10.19',
   install_requires = ["hypothesis", "stricttuple", "prettytable", "generators"],
   description = 'automated function fuzzer based on hypothesis to easily test production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/battle_tested',
-  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2017.10.18.1',
+  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2017.10.19',
   keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production', 'unittest'],
   classifiers = [],
 )
