80f4b1eecc50fc42b167ce751f834d319af4804e
Cody Kochmann <kochmanncody@gmail.com>
2019-06-21 12:01:25 -0400

    improved ram usage and single core support

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
+
+version = '2017.11.18.1',
+
 setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
-  version = '2017.11.18.1',
+  version = version,
   install_requires = ["hypothesis", "stricttuple", "prettytable", "generators", "strict_functions"],
-  description = 'automated function fuzzer based on hypothesis to easily test production code',
+  description = 'automated function and api fuzzer for easy testing of production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/battle_tested',
-  download_url = 'https://bit.ly/battle_tested_2017_11_18_1',
-  keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production', 'unittest'],
+  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/{}'.format(version),
+  keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production', 'unittest', 'api', 'fuzzer', 'stress'],
   classifiers = [],
 )
