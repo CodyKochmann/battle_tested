d9083a89bb97ecf89248a86093d743cf44b10017
CodyKochmann <kochmanncody@gmail.com>
2020-04-05 11:12:21 -0700

    updated setup.py to master version

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
+
 setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
-  version = '2017.11.18.1',
-  install_requires = ["hypothesis", "stricttuple", "prettytable", "generators", "graphdb", "strict_functions"],
-  description = 'automated function fuzzer based on hypothesis to easily test production code',
+  version = '2019.6.21',
+  install_requires = ["hypothesis", "stricttuple", "prettytable", "generators", "strict_functions"],
+  description = 'automated function and api fuzzer for easy testing of production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/battle_tested',
-  download_url = 'https://bit.ly/battle_tested_2017_11_18_1',
-  keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production', 'unittest'],
+  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2019.6.21',
+  keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production', 'unittest', 'api', 'fuzzer', 'stress'],
   classifiers = [],
 )
