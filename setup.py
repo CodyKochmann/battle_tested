bfccf5279e1cefbce5927b48f38fed1d0de272cc
Cody Kochmann <cody@thinkpad.home>
2020-09-07 05:11:26 -0700

    fixed hypothesis import

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'battle_tested',
   packages = ['battle_tested'], # this must be the same as the name above
-  version = '2019.6.21',
+  version = '2020.9.7',
   install_requires = ["hypothesis", "stricttuple", "prettytable", "generators", "strict_functions"],
   description = 'automated function and api fuzzer for easy testing of production code',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/battle_tested',
-  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2019.6.21',
+  download_url = 'https://github.com/CodyKochmann/battle_tested/tarball/2020.9.7',
   keywords = ['battle_tested', 'test', 'hypothesis', 'fuzzing', 'fuzz', 'production', 'unittest', 'api', 'fuzzer', 'stress'],
   classifiers = [],
 )
